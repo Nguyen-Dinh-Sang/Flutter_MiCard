03320f448cce00c8bca23078753f670fdd80894c
SSG\sangnd <sangnd@ssg.vn>
2021-06-14 16:42:09 +0700

    MiCard-DinhSang: test inspector

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: 50, horizontal: 30),
+            padding: EdgeInsets.all(30.0),
+            height: 200.0,
+            width: 200.0,
+            color: Colors.white,
+            child: Center(child: Text('Hello')),
+          ),
+        ),
+      ),
+    );
   }
 }
